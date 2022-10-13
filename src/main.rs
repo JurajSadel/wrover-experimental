da4f23c1c956780a7770aae26f649d7fd845b0e2
Juraj Sadel <juraj.sadel@espressif.com>
2022-10-13 10:50:09 +0200

    WIP: epaper support

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 #![feature(iter_advance_by)]
-use embedded_graphics::mono_font::iso_8859_2;
-use esp_idf_sys::time_t;
+use esp_idf_sys::{time_t, esp_light_sleep_start, esp_sleep_enable_timer_wakeup};
 use esp_idf_sys::{self as _};
 
 use embedded_svc::http::client::*;
@@ -15,11 +14,10 @@ use esp_idf_svc::sntp::SyncStatus;
 use esp_idf_svc::sysloop::*;
 use esp_idf_svc::wifi::*;
 
-use esp_idf_hal::delay;
+use esp_idf_hal::delay::{self, Ets};
 use esp_idf_hal::prelude::Peripherals;
-use esp_idf_hal::spi;
 
-use embedded_graphics::mono_font::{ascii::FONT_10X20, MonoTextStyle};
+use embedded_graphics::mono_font::{MonoTextStyleBuilder, MonoTextStyle};
 use embedded_graphics::pixelcolor::*;
 use embedded_graphics::prelude::*;
 use embedded_graphics::primitives::*;
@@ -27,16 +25,15 @@ use embedded_graphics::text::*;
 use embedded_text::{
     alignment::HorizontalAlignment,
     style::{HeightMode, TextBoxStyleBuilder},
-    TextBox,
 };
 
-use ili9341::Scroller;
+use esp_idf_hal::spi;
+use esp_idf_hal::spi::*;
 
-use regex::Regex;
+use esp_idf_hal::prelude::FromValueType;
 
 use std::ptr;
 use std::sync::Arc;
-use std::thread;
 use std::time::Duration;
 
 use time::format_description;
@@ -52,10 +49,15 @@ use ili9341::{self, Orientation};
 
 use display_interface_spi::SPIInterfaceNoCS;
 
-const SSID: &str = "Wokwi-GUEST";
-const PASS: &str = "";
+use epd_waveshare::{
+    color::*,
+    epd2in9_v2::{Display2in9, Epd2in9},
+    graphics::DisplayRotation,
+    prelude::*,
+};
 
-
+const SSID: &str = "test";
+const PASS: &str = "qwerqwer";
 
 fn main() -> anyhow::Result<()> {
     // Temporary. Will disappear once ESP-IDF 4.4 is released, but for now it is necessary to call this function once,
@@ -63,6 +65,7 @@ fn main() -> anyhow::Result<()> {
     esp_idf_sys::link_patches();
 
     esp_idf_svc::log::EspLogger::initialize_default();
+    info!("Start main");
 
     /*
     1 initialization(wifi, display, peripherals,....)
@@ -98,7 +101,7 @@ fn main() -> anyhow::Result<()> {
     let pins = peripherals.pins;
 
     // GPIOs for real HW wrover-kit
-    // let spi = peripherals.spi2; 
+    let mut spi = peripherals.spi2;
     // let backlight = pins.gpio5;
     // let dc = pins.gpio21;
     // let rst = pins.gpio18;
@@ -106,16 +109,25 @@ fn main() -> anyhow::Result<()> {
     // let miso = pins.gpio25;
     // let mosi = pins.gpio23;
     // let cs = pins.gpio22;
+    // let busy = pins.gpio4;
+
+    let cs = pins.gpio22;
+    let dc = pins.gpio21;
+    let sclk = pins.gpio19;
+    let rst = pins.gpio18;
+    let busy = pins.gpio5.into_input()?;
+    let mosi = pins.gpio23;
+    let miso = pins.gpio16;
 
     //GPIOs for Wokwi simulation
-    let spi = peripherals.spi3;
-    let backlight = pins.gpio5;
-    let dc = pins.gpio2;
-    let rst = pins.gpio4;
-    let sclk = pins.gpio18;
-    let miso = pins.gpio25;
-    let mosi = pins.gpio23;
-    let cs = pins.gpio15;
+    // let spi = peripherals.spi2;
+    // let backlight = pins.gpio5;
+    // let dc = pins.gpio21;
+    // let rst = pins.gpio18;
+    // let sclk = pins.gpio19;
+    // let miso = pins.gpio25;
+    // let mosi = pins.gpio23;
+    // let cs = pins.gpio22;
 
     // display
     let config = <spi::config::Config as Default>::default().baudrate((26_000_000).into());
@@ -125,37 +137,86 @@ fn main() -> anyhow::Result<()> {
 
     //https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/peripherals/spi_master.html
     //using real HW wrover-kit, please, use spi::Master::<spi::SPI2, _, _, _, _>::new instead of spi::Master::<spi::SPI3, _, _, _, _>::new
-    let di = SPIInterfaceNoCS::new(
-        spi::Master::<spi::SPI3, _, _, _, _>::new(
-            spi,
-            spi::Pins {
-                sclk,
-                sdo: mosi,
-                sdi: Some(miso),
-                cs: Some(cs),
-            },
-            config,
-        )?,
-        dc.into_output()?,
-    );
+    // let di = SPIInterfaceNoCS::new(
+    //     spi::Master::<spi::SPI2, _, _, _, _>::new(
+    //         spi,
+    //         spi::Pins {
+    //             sclk,
+    //             sdo: mosi,
+    //             sdi: Some(miso),
+    //             cs: Some(cs),
+    //         },
+    //         config,
+    //     )?,
+    //     dc.into_output()?,
+    // );
 
     let reset = rst.into_output()?;
-    let backlight = backlight.into_output()?;
+    //let backlight = backlight.into_output()?;
 
-    let mut display = ili9341::Ili9341::new(
-        di,
-        reset,
-        &mut delay::Ets,
-        Orientation::Portrait,
-        ili9341::DisplaySize240x320,
-    )
-    .map_err(|_| anyhow::anyhow!("Display"))?;
+    // let mut display = ili9341::Ili9341::new(
+    //     di,
+    //     reset,
+    //     &mut delay::Ets,
+    //     Orientation::Portrait,
+    //     ili9341::DisplaySize240x320,
+    // )
+    // .map_err(|_| anyhow::anyhow!("Display"))?;
 
-    draw_text(
-        &mut display,
-        &"".to_string(),
-        &"Initialization...".to_string(),
-    );
+    let config = config::Config::new().baudrate(26u32.MHz().into());
+    // let mut spi =
+    //     SPI2::new(spi, sclk, mosi, Some(busy), Some(cs), &config)?;
+
+    let mut delay: Ets = Ets::into(Ets);
+
+    let config = <spi::config::Config as Default>::default().baudrate(26.MHz().into());
+    let mut spi = spi::Master::<spi::SPI2, _, _, _, _>::new(
+        spi,
+        spi::Pins {
+            sclk,
+            sdo: mosi,
+            sdi: Some(miso),
+            cs: Some(pins.gpio15),
+        },
+        config,
+    )?;
+
+    let mut epd = Epd2in9::new(
+        &mut spi,
+        cs.into_output()?,
+        busy.into_input()?,
+        dc.into_output()?,
+        reset.into_output()?,
+        &mut delay).unwrap();
+
+    let mut display = Display2in9::default();
+
+
+    let mut display = Display2in9::default();
+    display.set_rotation(DisplayRotation::Rotate90);
+    println!("Color: {:?}, Is busy {:?}", epd.background_color(), epd.is_busy());
+
+    display.set_rotation(DisplayRotation::Rotate90);
+
+    draw_text_epaper(&mut display, " Hello Rust from ESP32dfg! ", 15, 50);
+    epd.update_and_display_frame(&mut spi, display.buffer(), &mut delay).expect("Frame cannot be cleared and updated!");
+
+    
+    /* 
+    unsafe {
+        esp_sleep_enable_timer_wakeup(60_000);
+        // match t {
+        //     Some(x) => { info!("Dobre je"); }
+        //     _ => { info!("Zle je"); }
+        // }
+        esp_light_sleep_start();
+    }
+*/
+    // draw_text_lcd(
+    //     &mut display,
+    //     &"".to_string(),
+    //     &"Initialization...".to_string(),
+    // );
 
     let url = String::from(
         "https://idos.idnes.cz/en/brno/odjezdy/vysledky/?f=Technologick%C3%BD%20park&fc=302003",
@@ -164,33 +225,30 @@ fn main() -> anyhow::Result<()> {
     let mut text_to_dislay: Vec<String> = Vec::new();
 
     loop {
-        info!("About to fetch content from {}", url);
+        info!("About to fetch content0 from {}", url);
 
         let mut client = EspHttpClient::new_default()?;
 
+        info!("About to fetch content1 from {}", url);
 
         let response = client.get(&url)?.submit()?;
 
+        info!("About to fetch content2 from {}", url);
 
         let body: Result<Vec<u8>, _> = Bytes::<_, 8>::new(response.reader()).collect();
 
+        info!("About to fetch content3 from {}", url);
+
         let body = body?;
         let str = String::from_utf8(body)?;
-        //let str = String::from_utf8_lossy(&body)?;
 
-        //let document = Html::parse_document(&body);
-        //let document = Dom::parse(&String::from_utf8_lossy(&body))?;
+        info!("About to fetch content4 from {}", url);
 
         let soup = Soup::new(&str);
-        //println!("Soup {:?}", soup);
-        //let times = soup.tag("table").find_all().nth(n);
 
         // display
 
         info!("About to initialize the ILI9341 SPI LED driver",);
-
-        //let mut scroller = display.configure_vertical_scroll(20, 5).map_err(|_| anyhow::anyhow!("Display"))?;
-        // display.scroll_vertically(scroller, 5);
 
         if text_to_dislay.is_empty() {
             let links = soup.tag("tr").find_all();
@@ -209,14 +267,12 @@ fn main() -> anyhow::Result<()> {
                         sign:mandatory]:[offset_minute]:[offset_second]",
                     )?;
 
-                    unsafe {
-                        let timer: *mut time_t = ptr::null_mut();
-                        text_to_dislay.push(format!(
-                            "{}:\n{}\n",
-                            &direction.unwrap(),
-                            &OffsetDateTime::parse(&time, &format)?.time().to_string()
-                        ));
-                    }
+                    let timer: *mut time_t = ptr::null_mut();
+                    text_to_dislay.push(format!(
+                        "{}:\n{}\n",
+                        &direction.unwrap(),
+                        &OffsetDateTime::parse(&time, &format)?.time().to_string()
+                    ));
                 } else {
                     continue;
                 }
@@ -233,7 +289,7 @@ fn main() -> anyhow::Result<()> {
             //     .time();
 
             //format!("Actual time: {}\n", &time.to_string())
-            draw_text(
+            draw_text_lcd(
                 &mut display,
                 &text_to_dislay.to_owned().into_iter().take(7).collect(),
                 &format!(
@@ -246,17 +302,43 @@ fn main() -> anyhow::Result<()> {
             );
         }
         text_to_dislay.clear();
-        thread::sleep(Duration::from_secs(10));
+        //thread::sleep(Duration::from_secs(10));
+        info!("About to sleep");
+        drop(wifi);
+        info!("Wifi stopped");
+        unsafe {
+            esp_sleep_enable_timer_wakeup(60_000);
+            // match t {
+            //     Some(x) => { info!("Dobre je"); }
+            //     _ => { info!("Zle je"); }
+            // }
+            esp_light_sleep_start();
+        }
+
+        // unsafe {
+        //     esp_deep_sleep(10_000_000);
+        // }
+
+        info!("About to wake up");
+        // let _wifi = wifi(
+        //     netif_stack.clone(),
+        //     sys_loop_stack.clone(),
+        //     default_nvs.clone(),
+        // )?;
     }
-    info!("About to sleep");
-    //thread::sleep(Duration::from_millis(1000));
-
-    drop(wifi);
-    info!("Wifi stopped");
-
-    Ok(())
 }
 
+fn draw_text_epaper(display: &mut Display2in9, text: &str, x: i32, y: i32) {
+    let style = MonoTextStyleBuilder::new()
+        .font(&embedded_graphics::mono_font::ascii::FONT_10X20)
+        .text_color(Black)
+        .background_color(White)
+        .build();
+
+    let text_style = TextStyleBuilder::new().baseline(Baseline::Top).build();
+
+    let _ = Text::with_text_style(text, Point::new(x, y), style, text_style).draw(display);
+}
 
 /*
 scan all accessible access points
@@ -303,8 +385,9 @@ fn wifi(
     Ok(wifi)
 }
 
+#[cfg(feature = "esp32_wrover_ili9341")]
 #[allow(dead_code)]
-fn draw_text<D>(display: &mut D, text: &String, time: &String) -> Result<(), D::Error>
+fn draw_text_lcd<D>(display: &mut D, text: &String, time: &String) -> Result<(), D::Error>
 where
     D: DrawTarget + Dimensions,
     D::Color: From<Rgb565>,
